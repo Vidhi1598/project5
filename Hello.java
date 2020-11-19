b4f7c78652a8384f1a7e3250297e8ce262108368
Vidhi1598 <vidhi998@gmail.com>
2020-11-19 18:34:29 +0530

    Hello

--- Hello.java
+++ Hello.java
@@ -5,22 +5,4 @@ public class Hello{
 		System.out.println("Hello1");
 		}
 }
-	
-=======
-import java.util.*;
-public class Hello{
-	
-	{
-		System.out.print("HEE");
-		
-	}
 
-	public static void main(String a[]){
-	System.out.print("Hello from saksham");
-	System.out.print("Hello from Vidhi");
-	
-	ArrayList al = new ArrayList();
-	}
-	
-}
->>>>>>> 1f60b7adeab97c34cca44fe7d606ea4454056acd
